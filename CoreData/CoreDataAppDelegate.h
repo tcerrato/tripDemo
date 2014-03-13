336250db71b917e73c4f641b29e1dfb390d96023
Tony Cerrato <cerrato_tony@Marybeth-Cerratos-iMac.local>
2014-03-13 15:05:33 -0400

    Initial Commit

---
+++ CoreData/CoreDataAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CoreDataAppDelegate.h
+//  CoreData
+//
+//  Created by Tony Cerrato on 3/13/14.
+//  Copyright (c) 2014 Tony Cerrato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreDataAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoreData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by Tony Cerrato on 3/13/14.
+//  Copyright (c) 2014 Tony Cerrato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CoreDataAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CoreDataAppDelegate class]));
+    }
+}
